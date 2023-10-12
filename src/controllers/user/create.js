1aafdde334d516c0f552c0e88557ddef8c956220
Ashley Patterson <apatterson0208@gmail.com>
2023-10-11 21:25:34 -0400

    Changed create controller for user

--- src/controllers/user/create.js
+++ src/controllers/user/create.js
@@ -2,11 +2,11 @@ const createUser = async (req, res) => {
   const {
     session, // this req.session property is put here by the handleCookieSessions middleware
     db: { User }, // this req.db.User property is put here by the addModelsToRequest middleware
-    body: { username, password }, // this req.body property is put here by the client
+    body: { username, password, email, first_name, last_name }, // this req.body property is put here by the client
   } = req;
 
   // TODO: check if username is taken, what should you return?
-  const user = await User.create(username, password);
+  const user = await User.create(username, password, email, first_name, last_name);
   session.userId = user.id;
 
   res.send(user);
